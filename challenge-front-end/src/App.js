0e29c402a6faabf8eecf2e05b0f5a6b8d4214062
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-26 16:47:40 -0300

    Add login page and css

--- challenge-front-end/src/App.js
+++ challenge-front-end/src/App.js
@@ -1,10 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 
 function App() {
   return (
     <>
-    <Login />
+      <Switch>
+        <Route exact path="/challenge-front-end">
+          <Login />
+        </Route>
+      </Switch>
     </>
   );
 }
--- challenge-front-end/src/pages/Login.jsx
+++ challenge-front-end/src/pages/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import LoginArea from '../components/LoginArea';
+import images from '../images/image-pokemon-color.png'
+import screenPokemon from '../images/login-screen-pokemons.png'
 
 export default class Login extends React.Component {
   render() {
     return (
-      <section>
-
+      <section className="section-login">
+        <img
+        className="image-logo-pokemon"
+        src={ images }
+        alt="Pokemon Logo" />
+        <LoginArea />
+        <img
+        className="screen-pokemons"
+        src={ screenPokemon }
+        alt="pokemons"
+        />
       </section>
     )
   }
