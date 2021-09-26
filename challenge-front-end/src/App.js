9a6dd977a1f774a0ce5269224f0c620d07dc3df8
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-26 17:49:17 -0300

    fixing all things

--- challenge-front-end/src/App.js
+++ challenge-front-end/src/App.js
@@ -1,12 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 
 function App() {
   return (
     <>
-    <Login />
+      <Switch>
+        <Route exact path="/challenge-front-end">
+          <Login />
+        </Route>
+      </Switch>
     </>
   );
 }
-
 export default App;
