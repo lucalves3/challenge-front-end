bb263ee64ca3185d84d7ef1f242b31af04f92f0c
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-27 15:15:27 -0300

    add All Pokemons

--- challenge-front-end/src/services/Api.js
+++ challenge-front-end/src/services/Api.js
@@ -1,5 +1,11 @@
-export async function getApiPokemon() {
-  const apiPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+export async function getAllPokemons() {
+  const apiPoke = await fetch('https://pokeapi.co/api/v2/pokemon?limit=900&offset=200')
+  const returnApiPoke = await apiPoke.json();
+  return returnApiPoke;
+}
+
+export async function getApiPokemon(namePokemon) {
+  const apiPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`);
   const apiToJson = await apiPoke.json();
   return apiToJson
 }
