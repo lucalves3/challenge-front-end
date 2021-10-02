e2f239573729e6c41bc9514db7f44d895149097e
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-10-02 14:25:00 -0300

    Fixing login page

--- challenge-front-end/src/components/LoginArea.jsx
+++ challenge-front-end/src/components/LoginArea.jsx
@@ -1,85 +1,112 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import vector from '../images/Vector.png'
+import vector from '../images/Vector.png';
 
 export default class LoginArea extends React.Component {
-  constructor() {
-    super()
+	constructor() {
+		super();
 
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handlePasswordChange = this.handlePasswordChange.bind(this);
+		this.handleButtonClick = this.handleButtonClick.bind(this);
+		this.checkLogin = this.checkLogin.bind(this);
 
-    this.state= {
-      emailArea: '',
-      passwordArea: '',
-    }
+		this.state= {
+			emailArea: '',
+			passwordArea: '',
+			getEmail: '',
+			getPassword: '',
+		};
     
-  }
+	}
+	componentDidMount() {
+		this.checkLogin();
+	}
 
-  handleEmailChange(event) {
-    this.setState({
-      emailArea: event.target.value
-    })
-  }
+	checkLogin() {
+		const { getEmail, getPassword } = this.state;
+		this.setState({
+			getEmail: localStorage.getItem('UserEmail'),
+			getPassword: localStorage.getItem('UserPassword')
+		});
+		const getInputEmail = document.getElementsByClassName('input-login-email');
+		const getInputPassword = document.getElementsByClassName('input-login-password');
+		console.log(getInputEmail.value);
+		if(getEmail !== '' && getPassword !== '') {
+			getInputEmail.value = getEmail;
+			getInputPassword.value = getPassword;
+		} 
+	}
 
-  handlePasswordChange(event) {
-    this.setState({
-      passwordArea: event.target.value
-    })
-  }
+	handleEmailChange(event) {
+		this.setState({
+			emailArea: event.target.value,
+		});
+	}
 
-  handleButtonClick() {
-    const email = this.state.emailArea;
-    const password = this.state.passwordArea;
-    localStorage.setItem(
-      'UserEmail', email,
-      )
-    localStorage.setItem(
-      'UserPassword', password,
-    )
-  }
+	handlePasswordChange(event) {
+		this.setState({
+			passwordArea: event.target.value,
+		});
 
-  render() {
-    return(
-      <section>
-        <form>
-          <h1 className="h1-login-area">Comece a coletar pokémons!</h1>
-          <input
-          className="input-login-email"
-          type="text"
-          required="required"
-          placeholder="Digite seu Email..."
-          onChange={ this.handleEmailChange }
-          />
-          <input
-          className="input-login-password"
-          type="password"
-          placeholder="Digite sua Senha..."
-          onChange={ this.handlePasswordChange }
-          />
-          <Link to="/vertodos">
-            <button
-            className="button-login" 
-            type="submit"
-            onClick={ this.handleButtonClick }
-            >
-              <p className="text-button">
+	}
+
+	handleButtonClick() {
+		const email = this.state.emailArea;
+		const password = this.state.passwordArea;
+		localStorage.setItem(
+			'UserEmail', email,
+		);
+		localStorage.setItem(
+			'UserPassword', password,
+		);
+	}
+
+	render() {
+		const { getEmail, getPassword } = this.state;
+		return(
+			<section>
+				<form>
+					<h1 className="h1-login-area">Comece a coletar pokémons!</h1>
+					<input
+						className="input-login-email"
+						type="text"
+						required
+						placeholder="Digite seu Email..."
+						onChange={ this.handleEmailChange }
+						value= { getEmail === '' ? this.handlePasswordChange : localStorage.getItem('UserEmail') }
+					/>
+					<input
+						className="input-login-password"
+						type="password"
+						required
+						placeholder="Digite sua Senha..."
+						onChange={ this.handlePasswordChange }
+						value={ getPassword === '' ? this.handlePasswordChange : localStorage.getItem('UserPassword') }
+					/>
+					<Link to="/vertodos">
+						<button
+							className="button-login" 
+							type="submit"
+							required
+							onClick={ this.handleButtonClick }
+						>
+							<p className="text-button">
                 Entrar
-              </p>
-            </button>
-          </Link>
-        </form>
-        <button className="button-change-theme">
-          <img
-          className="vector-theme-moon"
-          src={ vector }
-          alt="mudar de tema" />
-          <strong className="theme-text">
+							</p>
+						</button>
+					</Link>
+				</form>
+				<button className="button-change-theme">
+					<img
+						className="vector-theme-moon"
+						src={ vector }
+						alt="mudar de tema" />
+					<strong className="theme-text">
             Tema escuro
-          </strong>
-        </button>
-      </section>
-    )
-  }
+					</strong>
+				</button>
+			</section>
+		);
+	}
 }
